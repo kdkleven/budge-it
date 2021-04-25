70053c65c0eb11381433f571e3322f3567a99790
Kyle Kleven <kylekleven8@gmail.com>
2021-04-25 03:04:18 -0500

    Updated package.json

--- webpack.config.js
+++ webpack.config.js
@@ -28,21 +28,7 @@ const config = {
         }
       ]
     })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/, 
-        exclude: /node_modules/, 
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
+  ]
 };
 
 module.exports = config;
