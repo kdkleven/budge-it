073bff423fbef104e888c648dcfc4ad026efb041
Kyle Kleven <kylekleven8@gmail.com>
2021-04-24 20:34:52 -0500

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ const config = {
   mode: "development",
   plugins: [
     new WebpackPwaManifest({
-      name: "Budge-it Tracker",
-      short_name: "Budge-it",
-      description: "The best bang for your buck budget tracker.",
+      name: "Budge-it Tracker App",
+      short_name: "Budge-it App",
+      description: "The best bang for the buck budget tracker application.",
       background_color: "#01579b",
       theme_color: "#ffffff",
       start_url: "/",
@@ -20,59 +20,12 @@ const config = {
         {
           src: path.resolve("public/icons/icon-192x192.png"),
           sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join("icons")
-        }
-      ]
-    })
-  ],
-
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-
-const path = require("path");
-
-const config = {
-  entry: "./public/assets/js/app.js",
-  output: {
-    path: __dirname + "/public/dist",
-    filename: "bundle.js"
-  },
-  mode: "development",
-  plugins: [
-    new WebpackPwaManifest({
-      name: "Images App",
-      short_name: "Images App",
-      description: "An application for images",
-      background_color: "#01579b",
-      theme_color: "#ffffff",
-      "theme-color": "#ffffff",
-      start_url: "/",
-      icons: [
-        {
-          src: path.resolve("public/assets/images/icons/icon-192x192.png"),
-          sizes: [96, 128, 192, 256, 384, 512],
           destination: path.join("assets", "icons")
         }
       ]
     })
   ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // files must end in ".js" to be transpiled
-        exclude: /node_modules/, // don't transpile code from "node_modules"
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
-};
-
-module.exports = config;
-// configure webpack to use babel-loader to bundle our separate modules and transpile the code
+  // configure webpack to use babel-loader to bundle our separate modules and transpile the code
   // refer to https://github.com/babel/babel-loader for more information on the settings
   module: {
     rules: [
